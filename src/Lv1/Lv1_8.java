760f85cff19cb8879b3e6899b81566aaf6db3240
권태호 <taehokwon48@gmail.com>
2021-09-08 13:14:44 +0900

    Lv1_8

--- src/Lv1/Lv1_8.java
+++ src/Lv1/Lv1_8.java
@@ -1,8 +1,20 @@
 package Lv1;
 
+import java.util.Scanner;
+
 public class Lv1_8 {
 
     public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+        int total = 1;
+        for (int i = 1; i <= count; i++){
+            total = total * 2;
+            System.out.print(total+" ");
+
+        }
 
 
     }
