35884137bd07d3b5c00155e47aba8b6c43b6e27f
권태호 <taehokwon48@gmail.com>
2021-09-08 13:16:32 +0900

    Lv1_9

---
+++ src/Lv1/Lv1_9.java
@@ -0,0 +1,19 @@
+package Lv1;
+
+import java.util.Scanner;
+
+public class Lv1_9 {
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        for (int i = count; i >= 1; i--){
+            System.out.print(i+" ");
+        }
+
+
+    }
+
+}
