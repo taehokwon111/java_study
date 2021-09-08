90b8c275d186095d62a612a4fd2682611fd60c56
권태호 <taehokwon48@gmail.com>
2021-09-08 11:49:13 +0900

    Lv1_5

---
+++ src/Lv1/Lv1_5.java
@@ -0,0 +1,19 @@
+package Lv1;
+
+import java.util.Scanner;
+
+public class Lv1_5 {
+
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+        String count = sc.next();
+
+        for (int i = 0; i <= count.length(); i++){
+            char memory = count.charAt(count.length(i));
+            
+        }
+       
+    }
+
+}
